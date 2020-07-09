4180b32376539d4a357826699b9349f9d7587412
sohan110111 <sohan110111@gmail.com>
2020-07-09 12:09:43 +0600

    Now I am a react person

--- src/App.js
+++ src/App.js
@@ -3,13 +3,29 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // var person = {
+  //   name : "Dr. Mafuzar Rahman",
+  //   profession : "Professor"
+  // } 
+  // var person2 = {
+  //   name : "Eva Rahman",
+  //   job : "Singer"
+  // }
+  // var style = {
+  //   color : 'red',
+  //   backgroundColor : "yellow"
+  // }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <p>I am a react person</p>
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit done <code>src/App.js</code> and save to reload.
         </p>
+        <h1 className = "" style = {style}>Hello : {person.name + "\n" + person.profession} </h1>
+        <h3 style = {{backgroundColor: 'cyan'}}>Another Person : {person2.name + " " + person2.job}</h3>
+        <p>My first react Paragraph</p> */}
         {/* <a
           className="App-link"
           href="https://reactjs.org"

37f2cde662a9122b416dd52ec171682b0f06e01f
sohan110111 <sohan110111@gmail.com>
2020-07-09 23:47:51 +0600

    props

--- src/App.js
+++ src/App.js
@@ -15,13 +15,16 @@ function App() {
   //   color : 'red',
   //   backgroundColor : "yellow"
   // }
+  const nayoks = ["Anower", "Salman", "Faruk", "Dilshan"];
+  const nayika = ["Mousumi", "Purnima", "Emma", "Amily"];
   return (
     <div className="App">
       <header className="App-header">
         <p>I am a react person</p>
-        <Person></Person>
-        <Person></Person>
-        <Person></Person>
+        <Person nayok={nayoks[0]} nayika={nayika[0]}></Person>
+        <Person nayok={nayoks[1]} nayika={nayika[1]}></Person>
+        <Person nayok={nayoks[2]} nayika={nayika[2]}></Person>
+        <Person nayok={nayoks[3]} nayika={nayika[3]}></Person>
         {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit done <code>src/App.js</code> and save to reload.
@@ -41,15 +44,17 @@ function App() {
     </div>
   );
 }
-function Person() {
+function Person(props) {
   const personStyle = {
     border: '2px solid red',
     margin: '10px'
   }
+  console.log(props);
   return (
-  <div style = {personStyle}>
-    <h1>Name : Shakib</h1>
-    <h3>Hero of the year.</h3>
+  <div style = {personStyle}> 
+  {/* {border: '2px solid yellow', margin: "10px"} */}
+  <h1>Nayok : {props.nayok}</h1>
+    <h3>Hero of {props.nayika}.</h3>
   </div>
   )
 }
